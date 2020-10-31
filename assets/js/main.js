2371b3ea056195eecebe8cd1b6906de72f85a13f
shirejoni <hosseinshirjoni@chmail.ir>
2020-11-01 01:34:59 +0330

    Complete Menu Side

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,19 @@ let element = document.querySelector(".menu-icon");
 
 let toggleStatus = false;
 element.addEventListener('click', function () {
-    document.querySelector('body').classList.toggle('menu-expand');
-    toggleStatus = !toggleStatus;
+    console.log("Add Menu");
+    document.querySelector('body').classList.add('menu-expand');
+    toggleStatus = true;
 })
 
-document.addEventListener('click', function () {
+const clickOutSideHandler = (e) => {
+    let targetElement = e.target;
 
-});
+    if (!(e.target == element || document.querySelector('.menu-side').contains(targetElement))) {
+        document.querySelector('body').classList.remove('menu-expand');
+        toggleStatus = false;
+    }
+};
+
+document.addEventListener('click', clickOutSideHandler);
+
